a0e55d1e85cbbd562cac671f04bce5e673e7b068
Stefan Naumann <me@stefannaumann.de>
2016-04-16 17:56:33 +0200

    added SQLite support, multiple modes

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,5 +1,9 @@
 import Game
-import MyMaria
+import SQLite
+
+import Control.Monad 
+import Database.HDBC
+import Database.HDBC.Sqlite3
 
 inputNewGame :: IO(Game)
 inputNewGame = do putStrLn "Titel: "
@@ -10,14 +14,31 @@ inputNewGame = do putStrLn "Titel: "
                   p <- getLine
                   return (Game t d p)
 
-main :: IO([Game])
-main = do y <- inputNewGame
-          --printList [y]
-          insertGame y
-          x <- recvGameList
-          printList x
-          return ([y])
+editGame :: IO()
+editGame = return ()
 
---y <- recvGameList
---          printList y           
----          return ()
+--deleteGame :: IO()
+--deleteGame = return ()
+
+runAction :: String -> Connection -> IO()
+runAction action conn
+    | action == "a" = do y <- inputNewGame
+                         insertGame conn y
+                         return ()
+    | action == "d" = do printGameTable conn
+                         putStrLn "Welches Spiel löschen (int):"
+                         y <- getLine
+                         deleteGame conn (read y :: Integer)
+                         return ()
+    | action == "l" = do printGameTable conn
+                         return ()
+    | action == "e" = do return ()
+    | otherwise = return ()
+
+main :: IO()
+main = do conn <- connectSQLite "game.db"
+          putStrLn ("d <- delete\na <- add\ne <- edit\nl <- list")
+          action <- getLine
+          gamelist <- recvGameList conn
+          runAction action conn
+          closeSQLite conn
